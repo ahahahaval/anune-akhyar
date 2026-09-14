4348f675deb5067ab2fed03f0a444d0d2dbbfe4b
ahahahaval <naufalznime93@gmail.com>
2026-09-14 23:35:54 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const CONFIG = {
   url: "https://habkhyar.vercel.app/blog/membedah-gradient-descent",
 
   // Batasi jumlah percobaan untuk pengujian (default atau dari env)
- maxAttempts: 1000,
+ maxAttempts: 1,
 
 
   // Jeda antar-submisi dalam milidetik
